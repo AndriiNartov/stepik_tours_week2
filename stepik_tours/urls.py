f23670764dbe69ec1cdc3dab816402eecfc91c89
AndriiNartov <nartov1912@gmail.com>
2021-04-19 14:52:29 +0200

    Second commit

--- stepik_tours/urls.py
+++ stepik_tours/urls.py
@@ -1,11 +1,15 @@
 from django.contrib import admin
 from django.urls import path
 
-from tours import views
+from tours.views import main_view, departure_view, tour_view, custom_handler404, custom_handler500
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.main_view),
-    path('departure/<str:departure>/', views.departure_view),
-    path('tour/<int:id>/', views.tour_view),
+    path('', main_view),
+    path('departure/<str:departure>/', departure_view),
+    path('tour/<int:id>/', tour_view),
 ]
+
+
+handler404 = custom_handler404
+handler500 = custom_handler500
--- tours/views.py
+++ tours/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
-
+from django.http import HttpResponseNotFound, HttpResponseServerError
 
 # Create your views here.
+
+
 def main_view(request):
     return render(request, 'index.html')
 
@@ -12,3 +14,11 @@ def departure_view(request, departure):
 
 def tour_view(request, id):
     return render(request, 'tour.html')
+
+
+def custom_handler404(request, exception):
+    return HttpResponseNotFound('Здесь ничего нет')
+
+
+def custom_handler500(request):
+    return HttpResponseServerError('На сервере что-то сломалось')
