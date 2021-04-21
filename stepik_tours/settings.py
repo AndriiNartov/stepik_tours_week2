b4ecdd72166a9108318ad02b788e5d174dcd8124
AndriiNartov <80200103+AndriiNartov@users.noreply.github.com>
2021-04-21 14:29:57 +0200

    Update settings.py

--- stepik_tours/settings.py
+++ stepik_tours/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'n8cw^x(ws^eis)xct4f1rigx9^a5^ptm$!i+gd^^r)hjhvuc2s'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
